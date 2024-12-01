a054e515b2881d43ee324dff1e0941e83ad73e60
Stedd <toanpa+git@pm.me>
2024-12-01 12:37:18 +0100

    added timer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include <chrono>
+#include <iostream>
+
 #include "days/day01.h"
 
 int main()
 {
+	const auto startTime = std::chrono::high_resolution_clock::now();
 	day01::Run();
+	const auto endTime = std::chrono::high_resolution_clock::now();
+	const std::chrono::duration<double> diff = endTime - startTime;
+	std::cout << "Execution time: " << diff.count() * 1e6 << " μs" << std::endl;
 }
